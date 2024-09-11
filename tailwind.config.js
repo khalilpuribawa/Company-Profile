6d19dafbb349b3cd3e0137fc21f919da58e4adf9
khalilpuribawa <khalilpuribawa80@gmail.com>
2024-09-11 12:33:21 +0700

    Done

---
+++ tailwind.config.js
@@ -0,0 +1,35 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    container : {
+      center: true,
+      padding: '16px'
+    },
+    extend: {
+      colors:{
+        primary:'#2B3E75',
+        secondary:'#8071F2',
+        blue:'#6287F2',
+        hover:'#EAE5FF',
+        textfooter:'#667DBF'
+      },
+      strokeWidth: {
+        '1': '1',
+        '2': '2',
+        '3': '3',
+        '4': '4',
+        '5': '5',
+        // Tambahkan ketebalan stroke lainnya sesuai kebutuhan
+      },
+      boxShadow: {
+        'svg': '4px 4px 8px rgba(0, 0, 0, 0.3)',
+      },
+      screens:{
+        '2xl':'1320px'
+      }
+    },
+  },
+  plugins: [],
+}
+
